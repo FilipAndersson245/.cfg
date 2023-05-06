56e5bb6c24fb92c845581d6063d4bec18af77b66
Filip Andersson <17986183+FilipAndersson245@users.noreply.github.com>
2023-05-06 10:23:19 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,5 +10,5 @@ sudo apt -qq upgrade -y
 echo "Installing fish"
 sudo apt install fish -y
 
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/FilipAndersson245/.cfg/master/fish.fish | bash
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/FilipAndersson245/.cfg/master/fish.fish | fish
 
