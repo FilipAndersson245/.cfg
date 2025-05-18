c6c5ef1afb7fcde79ba0c4717c701c64b95c8311
Filip Andersson <17986183+FilipAndersson245@users.noreply.github.com>
2025-05-18 19:23:50 +0200

    Update setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,33 +1,3 @@
-function Add-PathToMachineEnvironment {
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$PathToAdd
-    )
-
-    # Ensure the path ends with a backslash
-    if (-not $PathToAdd.EndsWith("\")) {
-        $PathToAdd += "\"
-    }
-
-    # Get the current PATH environment variable for the machine
-    $currentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
-
-    # Check if the new path is already in the PATH variable
-    if ($currentPath -notlike "*$PathToAdd*") {
-        $newPathValue = "$currentPath;$PathToAdd"
-
-        try {
-            # Set the new PATH environment variable
-            [System.Environment]::SetEnvironmentVariable("Path", $newPathValue, [System.EnvironmentVariableTarget]::Machine)
-            Write-Output "The path '$PathToAdd' has been added successfully."
-        } catch {
-            Write-Error "Failed to set the environment variable: $_"
-        }
-    } else {
-        Write-Output "The path '$PathToAdd' is already in the PATH variable."
-    }
-}
-
 $devfolder = "C:\dev-setup\"
 $env:GIT_CONFIG  = $devfolder".gitconfig"
 $env:GIT_CONFIG_SYSTEM  = $devfolder".gitconfig"
@@ -39,22 +9,13 @@ $env:UV_INSTALL_DIR = $devfolder"uv"
 
 $env:UV_COMPILE_BYTECODE = "true"
 
-powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+$env:path = "$env:path;"$devfolder"cli\bin\""
 
-[System.Environment]::SetEnvironmentVariable('UV_PYTHON_INSTALL_DIR',"C:\dev-setup\uv\python", 'Machine')
-$env:UV_PYTHON_INSTALL_DIR = "C:\dev-setup\uv\python"
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 
 uv python install 3.13
 uv python install 3.12
 uv python install 3.10
-
-[System.Environment]::SetEnvironmentVariable('UV_CACHE_DIR',"C:\dev-setup\uv\.cache", 'Machine')
-$env:UV_CACHE_DIR = "C:\dev-setup\uv\.cache"
-
-[System.Environment]::SetEnvironmentVariable('UV_COMPILE_BYTECODE',"true", 'Machine')
-$env:UV_COMPILE_BYTECODE = "true"
-
-Add-PathToMachineEnvironment -PathToAdd "C:\dev-setup\cli\bin\"
 
 cargo-binstall --root C:\dev-setup\cli -y zoxide starship ripgrep bat sd hyperfine hexyl yazi-fm yazi-cli git-delta eza fd-find bottom du-dust just qsv xh samply miniserve gifski difftastic
 
