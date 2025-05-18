b2eab8d1fc8a656d32e340fdee7ad865ea9da953
Filip Andersson <17986183+FilipAndersson245@users.noreply.github.com>
2025-05-18 19:33:00 +0200

    Update setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,15 +1,17 @@
-$devfolder = "C:\dev-setup\"
-$env:GIT_CONFIG  = $devfolder".gitconfig"
-$env:GIT_CONFIG_SYSTEM  = $devfolder".gitconfig"
-$env:GIT_CONFIG_GLOBAL  = $devfolder".gitconfig"
-$env:CARGO_HOME = $devfolder".cargo"
-$env:UV_PYTHON_INSTALL_DIR = $devfolder"\uv\python"
-$env:UV_CACHE_DIR = $devfolder"uv\.cache"
-$env:UV_INSTALL_DIR = $devfolder"uv"
+$devfolder = "C:\dev-setup"
 
+$env:GIT_CONFIG = "$devfolder\.gitconfig"
+$env:GIT_CONFIG_SYSTEM = "$devfolder\.gitconfig"
+$env:GIT_CONFIG_GLOBAL = "$devfolder\.gitconfig"
+
+$env:CARGO_HOME = "$devfolder\.cargo"
+
+$env:UV_PYTHON_INSTALL_DIR = "$devfolder\uv\python"
+$env:UV_CACHE_DIR = "$devfolder\uv\.cache"
+$env:UV_INSTALL_DIR = "$devfolder\uv"
 $env:UV_COMPILE_BYTECODE = "true"
 
-$env:path = "$env:path;"$devfolder"cli\bin\""
+$env:path = "$env:path;$devfolder\cli\bin\"
 
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 
